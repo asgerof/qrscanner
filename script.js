ee0f3f2dbe319c2dd072f384da8d4d33529bb923
asgerof <asgerof@gmail.com>
2023-03-26 18:26:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const qrReader = document.getElementById('qr-reader');
-    const result = document.getElementById('result');
+document.addEventListener("DOMContentLoaded", function () {
+  const qrReader = document.getElementById("qr-reader");
+  const result = document.getElementById("result");
 
-    function onScanSuccess(decodedText) {
-        result.textContent = decodedText;
+  function onScanSuccess(decodedText) {
+    result.textContent = decodedText;
+  }
+
+  function onScanFailure(error) {
+    console.warn(`QR code scanning failed: ${error}`);
+  }
+
+  async function getBackCamera() {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    return devices.find((device) => device.kind === "videoinput" && device.label.toLowerCase().includes("back"));
+  }
+
+  async function initializeScanner() {
+    const backCamera = await getBackCamera();
+    const config = {
+      fps: 10,
+      qrbox: { width: 250, height: 250 },
+    };
+
+    if (backCamera) {
+      config.videoConstraints = {
+        deviceId: backCamera.deviceId,
+        facingMode: "environment",
+      };
     }
 
-    function onScanFailure(error) {
-        console.warn(`QR code scanning failed: ${error}`);
-    }
-
-    const html5QrcodeScanner = new Html5QrcodeScanner(
-        'qr-reader',
-        { fps: 10, qrbox: { width: 250, height: 250 } },
-        /* verbose= */ false
-    );
-
+    const html5QrcodeScanner = new Html5QrcodeScanner("qr-reader", config, /* verbose= */ false);
     html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+  }
+
+  initializeScanner();
 });
