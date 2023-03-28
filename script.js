fd5a3c02c450f4ae0706e2d0b2d35fe7456bb4fd
asgerof <asgerof@gmail.com>
2023-03-28 18:57:21 +0200

    asd

--- script.js
+++ script.js
@@ -6,23 +6,23 @@ document.addEventListener("DOMContentLoaded", function () {
   let contestants = [];
 
   function onScanSuccess(decodedText) {
-  result.textContent = decodedText;
+    result.textContent = decodedText;
 
-  const lowerCaseDecodedText = decodedText.toLowerCase();
+    const lowerCaseDecodedText = decodedText.toLowerCase();
 
-  if (!isScanningContestant) {
-    const conttypePattern = /^conttype:\s*([^;]+)\s*;\s*(\d+)$/i;
-    const match = decodedText.match(conttypePattern);
-    if (match) {
-      const contestType = match[1].trim();
-      const contestantAmount = parseInt(match[2]);
+    if (!isScanningContestant) {
+      const conttypePattern = /^conttype:\s*([^;]+)\s*;\s*(\d+)$/i;
+      const match = decodedText.match(conttypePattern);
+      if (match) {
+        const contestType = match[1];
+        const contestantAmount = parseInt(match[2]);
 
-      const headerText = document.getElementById("header-text");
-      headerText.textContent = contestType;
+        const headerText = document.getElementById("header-text");
+        headerText.textContent = contestType;
 
-      createContestantLabels(contestantAmount);
-      document.getElementById("add-contestant").disabled = false;
-    }
+        createContestantLabels(contestantAmount);
+        document.getElementById("add-contestant").disabled = false;
+      }
     } else {
       const contestantPrefix = "contestant:";
       if (lowerCaseDecodedText.startsWith(contestantPrefix)) {
@@ -73,41 +73,40 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   function createContestantLabels(amount) {
-  const container = document.getElementById("contestants-container");
-  container.innerHTML = "";
+    const container = document.getElementById("contestants-container");
+    container.innerHTML = "";
 
-  for (let i = 1; i <= amount; i++) {
-    const label = document.createElement("span");
-    label.textContent = `Contestant ${i}`;
-    label.id = `contestant-${i}`;
+    for (let i = 1; i <= amount; i++) {
+      const label = document.createElement("span");
+      label.textContent = `Contestant ${i}`;
+      label.id = `contestant-${i}`;
 
-    const trashIcon = document.createElement("button");
-    trashIcon.textContent = "🗑";
-    trashIcon.onclick = function () {
-      resetContestantLabel(i);
-    };
+      const trashIcon = document.createElement("button");
+      trashIcon.textContent = "🗑";
+      trashIcon.onclick = function () {
+        resetContestantLabel(i);
+      };
 
-    const wrapper = document.createElement("div");
-    wrapper.appendChild(label);
-    wrapper.appendChild(trashIcon);
+      const wrapper = document.createElement("div");
+      wrapper.appendChild(label);
+      wrapper.appendChild(trashIcon);
 
-    container.appendChild(wrapper);
+      container.appendChild(wrapper);
 
-    contestants[i - 1] = {
-      labelElement: label,
-      name: "",
-    };
+      contestants[i - 1] = {
+        labelElement: label,
+        name: "",
+      };
+    }
   }
-}
 
-function addContestantName(name) {
+  function addContestantName(name) {
   const availableContestant = contestants.find((contestant) => !contestant.name);
   if (availableContestant) {
     availableContestant.labelElement.textContent = name;
     availableContestant.name = name;
   }
 }
-
 function resetContestantLabel(index) {
   const contestant = contestants[index - 1];
   contestant.labelElement.textContent = `Contestant ${index}`;
