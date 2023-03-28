6b7dbb9d03eb7a6f3b35ba63c4b39dedac7de593
asgerof <asgerof@gmail.com>
2023-03-28 18:40:24 +0200

    sadf

--- script.js
+++ script.js
@@ -2,16 +2,37 @@ document.addEventListener("DOMContentLoaded", function () {
   const qrReader = document.getElementById("qr-reader");
   const result = document.getElementById("result");
 
-function onScanSuccess(decodedText) {
-  result.textContent = decodedText;
+  let isScanningContestant = false;
+  let contestants = [];
 
-  const conttypePrefix = "conttype:";
-  if (decodedText.toLowerCase().startsWith(conttypePrefix)) {
-    const contestType = decodedText.slice(conttypePrefix.length).trim();
-    const headerText = document.getElementById("header-text");
-    headerText.textContent = contestType;
+  function onScanSuccess(decodedText) {
+    result.textContent = decodedText;
+
+    const lowerCaseDecodedText = decodedText.toLowerCase();
+
+    if (!isScanningContestant) {
+      const conttypePattern = /^conttype:\s*([^;]+);\s*(\d+)$/i;
+      const match = decodedText.match(conttypePattern);
+      if (match) {
+        const contestType = match[1];
+        const contestantAmount = parseInt(match[2]);
+
+        const headerText = document.getElementById("header-text");
+        headerText.textContent = contestType;
+
+        createContestantLabels(contestantAmount);
+        document.getElementById("add-contestant").disabled = false;
+      }
+    } else {
+      const contestantPrefix = "contestant:";
+      if (lowerCaseDecodedText.startsWith(contestantPrefix)) {
+        const contestantName = decodedText.slice(contestantPrefix.length).trim();
+        addContestantName(contestantName);
+      }
+      isScanningContestant = false;
+      html5QrcodeScanner.clear();
+    }
   }
-}
 
   function onScanFailure(error) {
     console.warn(`QR code scanning failed: ${error}`);
@@ -36,14 +57,59 @@ function onScanSuccess(decodedText) {
       };
     }
 
-    const html5QrcodeScanner = new Html5QrcodeScanner("qr-reader", config, /* verbose= */ false);
+    html5QrcodeScanner = new Html5QrcodeScanner("qr-reader", config, /* verbose= */ false);
 
     const startScanningButton = document.getElementById("start-scanning");
     startScanningButton.addEventListener("click", () => {
       html5QrcodeScanner.render(onScanSuccess, onScanFailure);
       startScanningButton.disabled = true;
     });
+
+    const addContestantButton = document.getElementById("add-contestant");
+    addContestantButton.addEventListener("click", () => {
+      isScanningContestant = true;
+      html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+    });
   }
 
-  initializeScanner();
-});
+  function createContestantLabels(amount) {
+  const container = document.getElementById("contestants-container");
+  container.innerHTML = "";
+
+  for (let i = 1; i <= amount; i++) {
+    const label = document.createElement("span");
+    label.textContent = `Contestant ${i}`;
+    label.id = `contestant-${i}`;
+
+    const trashIcon = document.createElement("button");
+    trashIcon.textContent = "🗑";
+    trashIcon.onclick = function () {
+      resetContestantLabel(i);
+    };
+
+    const wrapper = document.createElement("div");
+    wrapper.appendChild(label);
+    wrapper.appendChild(trashIcon);
+
+    container.appendChild(wrapper);
+
+    contestants[i - 1] = {
+      labelElement: label,
+      name: "",
+    };
+  }
+}
+
+function addContestantName(name) {
+  const availableContestant = contestants.find((contestant) => !contestant.name);
+  if (availableContestant) {
+    availableContestant.labelElement.textContent = name;
+    availableContestant.name = name;
+  }
+}
+
+function resetContestantLabel(index) {
+  const contestant = contestants[index - 1];
+  contestant.labelElement.textContent = `Contestant ${index}`;
+  contestant.name = "";
+}
